7f02edc682bd0c4984bc91cb26ee70882baed14e
Brian Hechinger <wonko@4amlunch.net>
2018-01-31 08:53:36 -0500

    Let's throw errors.

--- handler/Permissions.go
+++ handler/Permissions.go
@@ -4,6 +4,7 @@ import (
 	"github.com/chremoas/role-srv/proto"
 	"github.com/micro/go-micro/client"
 	"golang.org/x/net/context"
+	"errors"
 )
 
 type permissionsHandler struct {
@@ -15,17 +16,17 @@ func NewPermissionsHandler() chremoas_role.PermissionsHandler {
 }
 
 func (h *permissionsHandler) Perform(ctx context.Context, request *chremoas_role.PermissionsRequest, response *chremoas_role.PerformResponse) error {
-	return nil
+	return errors.New("Not Implemented")
 }
 
 func (h *permissionsHandler) AddPermisssions(ctx context.Context, request *chremoas_role.PermissionsRequest, response *chremoas_role.PermissionsResponse) error {
-	return nil
+	return errors.New("Not Implemented")
 }
 
 func (h *permissionsHandler) RemovePermissions(ctx context.Context, request *chremoas_role.PermissionsRequest, response *chremoas_role.PermissionsResponse) error {
-	return nil
+	return errors.New("Not Implemented")
 }
 
 func (h *permissionsHandler) GetPermissions(ctx context.Context, request *chremoas_role.PermissionsRequest, response *chremoas_role.PermissionsResponse) error {
-	return nil
+	return errors.New("Not Implemented")
 }
