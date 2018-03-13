c04fb9ef5b4ae91dd4297be8a8a828bd50c95963
Brian Hechinger <wonko@4amlunch.net>
2018-03-13 15:31:01 -0400

    Getting config from redis. Removed debugging stuff. changed redis key generation.

--- main.go
+++ main.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"fmt"
-	//uauthsvc "github.com/chremoas/auth-srv/proto"
-	//discord "github.com/chremoas/discord-gateway/proto"
 	"github.com/chremoas/role-srv/handler"
 	"github.com/chremoas/role-srv/proto"
 	"github.com/chremoas/services-common/config"
 	"github.com/micro/go-micro"
-	//"github.com/micro/go-micro/client"
 )
 
 var Version = "1.0.0"
 var service micro.Service
 var name = "role"
-var conf *config.Configuration
 
 func main() {
 	service = config.NewService(Version, "srv", name, initialize)
